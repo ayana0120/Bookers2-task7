07e3a1da6b472fa2336f44d600396cf85b7d3afb
ayana0120 <keichan0501.ayana@gmail.com>
2020-11-22 16:51:24 +0900

    [Add]jpostal.jpとjp_prefectureを導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+$(function() {
+  return $('#user_postcode').jpostal({
+    postcode: ['#user_postcode'],
+    address: {
+      '#user_prefecture_code': '%3',
+      '#user_city': '%4',
+      '#user_street': '%5%6%7',
+    },
+  });
+});
